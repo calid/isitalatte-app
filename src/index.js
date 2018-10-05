220033f7fd9bd83b73b25a9295611ca13af6001b
Dylan Cali <calid1984@gmail.com>
2018-10-05 13:03:54 -0400

    add image upload functionality

--- src/index.js
+++ src/index.js
@@ -5,10 +5,38 @@ import 'github-fork-ribbon-css/gh-fork-ribbon.css'
 import './index.css'
 
 class LatteClassifier extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      imageUrl: ''
+    }
+  }
+
   onImageDrop (acceptedFiles, rejectedFiles) {
+    const imageFile = acceptedFiles[0]
+    const reader = new window.FileReader()
+
+    reader.onloadend = () => {
+      this.setState({
+        imageUrl: reader.result
+      })
+
+      console.log(this.state.imageUrl)
+    }
+
+    reader.readAsDataURL(imageFile)
   }
 
   render () {
+    const { imageUrl } = this.state
+    let image = null
+
+    if (imageUrl) {
+      image = (<img className='target-image' src={imageUrl} />)
+    } else {
+      image = (<div />)
+    }
+
     return (
       <div className='latte-classifier'>
         <a
@@ -19,6 +47,7 @@ class LatteClassifier extends React.Component {
           Fork me on GitHub
         </a>
         <h1 className='title-banner'>Is it a latte?</h1>
+        {image}
         <Dropzone
           className='dropzone'
           accept='image/*'
@@ -29,6 +58,7 @@ class LatteClassifier extends React.Component {
     )
   }
 }
+
 ReactDOM.render(
   <LatteClassifier />,
   document.getElementById('root')
